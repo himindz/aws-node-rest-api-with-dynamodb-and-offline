ea96b33491d0362147eb8ac504eac3f51914079a
Ali Khan <alikhan@himindz.ie>
2017-08-29 21:23:30 +0100

    update

--- todos/dynamodb.js
+++ todos/dynamodb.js
@@ -5,12 +5,10 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 let options = {};
 
 // connect to local DB if running offline
-if (process.env.IS_OFFLINE) {
   options = {
     region: 'localhost',
-    endpoint: 'http://localhost:8000',
+    endpoint: 'http://172.17.0.1:8000',
   };
-}
 
 const client = new AWS.DynamoDB.DocumentClient(options);
 
